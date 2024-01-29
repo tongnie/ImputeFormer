09b35b5be2987a7d4ce3abc525eb4b684b02f19a
Tong Nie <1854361@tongji.edu.cn>
2024-01-29 20:47:04 +0800

    Add files via upload

---
+++ Attention_layers.py
@@ -0,0 +1,128 @@
+import torch.nn as nn
+import torch
+
+
+
+class AttentionLayer(nn.Module):
+    """
+    Multi-head scaled dot attention
+    """
+    def __init__(self, model_dim, num_heads=8, mask=False):
+        super().__init__()
+
+        self.model_dim = model_dim
+        self.num_heads = num_heads
+        self.mask = mask
+        self.head_dim = model_dim // num_heads
+
+        self.FC_Q = nn.Linear(model_dim, model_dim)
+        self.FC_K = nn.Linear(model_dim, model_dim)
+        self.FC_V = nn.Linear(model_dim, model_dim)
+
+        self.out_proj = nn.Linear(model_dim, model_dim)
+
+    def forward(self, query, key, value):
+        # Q    (batch_size, ..., tgt_length, model_dim)
+        # K, V (batch_size, ..., src_length, model_dim)
+        batch_size = query.shape[0]
+        tgt_length = query.shape[-2]
+        src_length = key.shape[-2]
+
+        query = self.FC_Q(query)
+        key = self.FC_K(key)
+        value = self.FC_V(value)
+
+        # Qhead, Khead, Vhead (num_heads * batch_size, ..., length, head_dim)
+        query = torch.cat(torch.split(query, self.head_dim, dim=-1), dim=0)
+        key = torch.cat(torch.split(key, self.head_dim, dim=-1), dim=0)
+        value = torch.cat(torch.split(value, self.head_dim, dim=-1), dim=0)
+
+        key = key.transpose(-1, -2)  # (num_heads * batch_size, ..., head_dim, src_length)
+
+        attn_score = (
+            query @ key
+        ) / self.head_dim**0.5  # (num_heads * batch_size, ..., tgt_length, src_length)
+
+        if self.mask:
+            mask = torch.ones(
+                tgt_length, src_length, dtype=torch.bool, device=query.device
+            ).tril()  # lower triangular part of the matrix
+            attn_score.masked_fill_(~mask, -torch.inf)  # fill in-place
+
+        attn_score = torch.softmax(attn_score, dim=-1)
+        out = attn_score @ value  # (num_heads * batch_size, ..., tgt_length, head_dim)
+        out = torch.cat(
+            torch.split(out, batch_size, dim=0), dim=-1
+        )  # (batch_size, ..., tgt_length, head_dim * num_heads = model_dim)
+
+        out = self.out_proj(out)
+
+        return out
+
+
+class SelfAttentionLayer(nn.Module):
+    """
+    Canonical self-attention layer
+    """
+    def __init__(self,
+                 model_dim, feed_forward_dim=2048, num_heads=8, dropout=0, mask=False):
+        super().__init__()
+
+        self.attn = AttentionLayer(model_dim, num_heads, mask)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(model_dim, feed_forward_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(feed_forward_dim, model_dim),
+        )
+        self.ln1 = nn.LayerNorm(model_dim)
+        self.ln2 = nn.LayerNorm(model_dim)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x, dim=-2):
+        x = x.transpose(dim, -2)
+        # x: (batch_size, ..., length, model_dim)
+        residual = x
+        out = self.attn(x, x, x)  # (batch_size, ..., length, model_dim)
+        out = self.dropout1(out)
+        out = self.ln1(residual + out)
+
+        residual = out
+        out = self.feed_forward(out)  # (batch_size, ..., length, model_dim)
+        out = self.dropout2(out)
+        out = self.ln2(residual + out)
+
+        out = out.transpose(dim, -2)
+        return out
+
+
+class EmbeddedAttention(nn.Module):
+    """
+    Spatial embedded attention layer
+    """
+    def __init__(self, model_dim, adaptive_embedding_dim):
+        super().__init__()
+
+        self.model_dim = model_dim
+
+        self.FC_Q_K = nn.Linear(adaptive_embedding_dim, model_dim)
+        self.FC_V = nn.Linear(model_dim, model_dim)
+        self.out_proj = nn.Linear(model_dim, model_dim)
+
+    def forward(self, value, emb):
+        # V (batch_size, ..., seq_length, model_dim)
+        # emb (..., length, model_dim)
+        batch_size = value.shape[0]
+        query = self.FC_Q_K(emb)
+        key = self.FC_Q_K(emb)
+        value = self.FC_V(value)
+
+        # Q, K (..., length, model_dim)
+        # V (batch_size, ..., length, model_dim)
+        key = key.transpose(-1, -2)  # (..., model_dim, src_length)
+        attn_score = query @ key  # (..., tgt_length, src_length)
+        attn_score = torch.softmax(attn_score, dim=-1)
+        attn_score = repeat(attn_score, 'n s1 s2 -> b n s1 s2', b=batch_size)
+        out = attn_score @ value  # (batch_size, ..., tgt_length, model_dim)
+
+        return out
